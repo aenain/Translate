4905d031ffa80ff88930860a37448125f30f01ee
Artur Hebda <artur@adtaily.com>
2011-09-14 11:50:58 +0200

    Creation of application and setting up haml with coffeescript.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Translate::Application.config.secret_token = '52568b595a3a5c0d2cebeb23664cea7c0a7bf964d1e3f63b17e18c42d1ce43265f18d6ef24b4d69e29767587e327b9db964500929cf7ef510bf86247e9faab27'

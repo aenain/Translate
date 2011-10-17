78f93c0a7b5b721efee88cf65de7a17d2936f203
Artur Hebda <artur@adtaily.com>
2011-10-17 11:12:03 +0200

    Wyrzucenie migracji zmieniających collation na utf8_bin z utf8_unicode_ci.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111016144800) do
+ActiveRecord::Schema.define(:version => 20111016144613) do
 
   create_table "exam_entries", :force => true do |t|
     t.integer  "exam_id"

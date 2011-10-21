f7288cc738e04a59e0be333d7d3f8e6c38c7814e
Artur Hebda <artur@adtaily.com>
2011-10-21 17:21:15 +0200

    Poprawka przy migracji zmieniającej maila admina.

--- db/migrate/20111021144923_change_admin_mail_and_password.rb
+++ db/migrate/20111021144923_change_admin_mail_and_password.rb
@@ -2,10 +2,12 @@ class ChangeAdminMailAndPassword < ActiveRecord::Migration
   def up
     @admin = AdminUser.find_by_email('admin@example.com')
 
-    @admin.email = "arturhebda@gmail.com"
-    @admin.password = "Tr@ns!@Te"
+    if @admin.present?
+      @admin.email = "arturhebda@gmail.com"
+      @admin.password = "Tr@ns!@Te"
 
-    @admin.save!
+      @admin.save!
+    end
   end
 
   def down

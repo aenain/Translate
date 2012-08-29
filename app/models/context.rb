9208e6a5e2c81b3895549e1a1889edb77c7f39a6
Artur Hebda <artur@adtaily.com>
2012-08-29 18:02:33 +0200

    Implementing splitting contexts into parts to fill + tests.

--- app/models/context.rb
+++ app/models/context.rb
@@ -6,5 +6,24 @@ class Context < ActiveRecord::Base
   has_one :word, through: :translating, source: :original, class_name: Word.name
   has_one :translated_word, through: :translating, source: :translated, class_name: Word.name
 
-  validates :sentence, presence: true
+  validates :sentence, presence: true, length: { minimum: 5 }
+
+  # returns as array of strings fragments of sentence which are expected to be filled out in quiz
+  def parts_to_fill
+    parts = []
+    sentence.scan(/(?:\/)([^\/]+)(?:\/)/) { |group| parts << group[0] }
+    parts
+  end
+
+  # returns sentence in the certain formats:
+  # :raw (def.) - exactly what's in database
+  # :clear - remove fill markers and returns clear sentence
+  def sentence(format = :raw)
+    raw_sentence = read_attribute(:sentence)
+
+    case format
+      when :raw then raw_sentence
+      when :clear then raw_sentence.gsub(FILL_DELIMITER, '')
+    end
+  end
 end
---
+++ test/unit/context_test.rb
@@ -0,0 +1,66 @@
+# encoding: utf-8
+require 'test_helper'
+
+class ContextTest < ActiveSupport::TestCase
+  should have_one(:translating)
+  should have_one(:translation).through(:translating)
+  should have_one(:word).through(:translating)
+
+  should have_db_column(:sentence).of_type(:string)
+
+  context ".create" do
+    context "context without sentence" do
+      setup do
+        @context = Context.create(sentence: nil)
+      end
+
+      should "not save the context" do
+        assert @context.new_record?
+      end
+    end
+
+    context "context without sentence" do
+      setup do
+        @context = Context.create(sentence: nil)
+      end
+
+      should "not save the context" do
+        assert @context.new_record?
+      end
+    end
+  end
+
+  context "#parts_to_fill" do
+    setup do
+      @contexts = {
+        none_to_fill: FactoryGirl.create(:context, sentence: "It's simple as hell."),
+        one_to_fill: FactoryGirl.create(:context, sentence: "When I was /in/ San Francisco, it was raining outside."),
+        more_to_fill: FactoryGirl.create(:context, sentence: "I /know/ it seems to be /funny/."),
+      }
+    end
+
+    should "extract fragments to fill properly" do
+      assert_equal [], @contexts[:none_to_fill].parts_to_fill
+      assert_equal ['in'], @contexts[:one_to_fill].parts_to_fill
+      assert_equal %w[know funny], @contexts[:more_to_fill].parts_to_fill
+    end
+  end
+
+  context "#sentence" do
+    setup do
+      @context = FactoryGirl.create(:context, sentence: "I /know/ it seems to be /funny/.")
+    end
+
+    should "return raw value from database if no arguments specified" do
+      assert_equal "I /know/ it seems to be /funny/.", @context.sentence
+    end
+
+    should "return raw value from database if :raw specified" do
+      assert_equal "I /know/ it seems to be /funny/.", @context.sentence(:raw)
+    end
+
+    should "return a sentence without fill marks if :no_fill_markers specified" do
+      assert_equal "I know it seems to be funny.", @context.sentence(:clear)
+    end
+  end
+end
